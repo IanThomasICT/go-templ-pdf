f38731f13e3e26bdb045b28da94dd80aed8dc2d6
Ian Thomas <ianthomasict@gmail.com>
2024-01-11 00:27:46 -0600

    feat: pdf generation working, added makefile

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 
+	"github.com/a-h/templ"
 	"github.com/go-rod/rod"
 	"github.com/ianthomasict/go-templ-pdf/src/reports"
 	"github.com/ianthomasict/go-templ-pdf/src/view"
@@ -14,8 +16,30 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type TemplateLoader struct {
+	ctx context.Context
+	buf *bytes.Buffer
+}
+
+func (t *TemplateLoader) load(tmpl templ.Component) {
+	tmpl.Render(t.ctx, t.buf)
+}
+
+func (t *TemplateLoader) toPDF() ([]byte, error) {
+	html := t.buf.String()
+
+	page := rod.New().MustConnect().MustPage()
+	err := page.SetDocumentContent(html)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to set page html content: %w", err)
+	}
+	pdfBytes := page.MustPDF()
+
+	return pdfBytes, nil
+}
+
 func main() {
-	srv := echo.New();
+	srv := echo.New()
 
 	invoices := []view.Invoice{
 		{Name: "Roy's Solutions", Total: 1022.53},
@@ -28,36 +52,29 @@ func main() {
 
 	srv.GET("/", func(ctx echo.Context) error {
 		return view.Page(invoices).Render(ctx.Request().Context(), ctx.Response().Writer)
-	}) 
+	})
 
 	api := srv.Group("/api")
 	api.GET("/download/:id", func(c echo.Context) error {
-		id, err := strconv.ParseInt(c.Param("id"), 10, 0); 
-		if err != nil {
+		id, err := strconv.ParseInt(c.Param("id"), 10, 0)
+		if err != nil || id == 0 {
 			c.Logger().Error(err)
-			return c.JSON(http.StatusBadRequest, "Missing valid 'id' param")
+			return c.JSON(http.StatusBadRequest, fmt.Sprintf("Missing valid 'id' param: %v", id))
 		}
 		name := c.QueryParam("name")
-		if id == 0 || name == "" {
-			c.Logger().Error(err)
-			return c.JSON(400, "missing required parameter");
+		if name == "" {
+			c.Logger().Error("Missing required parameter: 'name'")
+			return c.JSON(400, "Missing required parameter: 'name'")
 		}
 
-		// html, err := templ.ToGoHTML(c.Request().Context(), reports.MainReport(name, id))
-		// if err != nil {
-		// 	return c.JSON(500, "An error occurred while generating report contents")
-		// }
+		templater := TemplateLoader{c.Request().Context(), new(bytes.Buffer)}
+		templater.load(reports.MainReport(name, id))
 
-		htmlBuf := new(bytes.Buffer)
-		reports.MainReport(name,id).Render(c.Request().Context(), htmlBuf)
-		fmt.Println(htmlBuf.String()[:100])
-
-		pdfBytes, err := generatePDF(c, htmlBuf.String())
+		pdfBytes, err := templater.toPDF()
 		if err != nil {
 			c.Logger().Error(err)
 			return c.JSON(500, "An error occurred while generating PDF")
 		}
-
 
 		return c.Blob(200, "application/pdf", pdfBytes)
 	})
@@ -65,19 +82,3 @@ func main() {
 	log.Println("Starting server on port 8080!")
 	log.Fatalln(http.ListenAndServe(":8080", srv))
 }
-
-
-
-func generatePDF(c echo.Context, html string) ([]byte, error) {
-	c.Logger().Info("Initializing page")
-	page := rod.New().MustConnect().MustPage("http://google.com")
-	c.Logger().Info("Setting content")
-	err := page.SetDocumentContent(html)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to set page html content: %w", err)
-	}
-	pdfBytes := page.MustPDF("") ;
-
-	return pdfBytes, nil;
-}
-
